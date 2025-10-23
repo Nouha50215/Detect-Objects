ffe784abf4507f79922adaba5b00e2d37ed64601
ryu ashi <ryu.ashi.502@gmail.com>
2025-10-22 23:00:51 -0500

    Introduction

--- python.py
+++ python.py
@@ -1,1 +1,14 @@
 # Object Detection in Python (SPP-- Using ML and OpenCV)
+
+# Load the image 
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("image.jpg") #Reads the image.
+
+
+# Converting Image Color Formats
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert BGR to RGB and BGR to grayscale/ from one color space to another 
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(img_rgb)
+plt.show()
